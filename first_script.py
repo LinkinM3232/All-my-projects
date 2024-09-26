36770465267cad8427d60348b67da068d3d5b30f
User <maximkleynovskiy@gmail.com>
2024-09-26 19:45:52 +0300

    Добавление файла

--- first_script.py
+++ first_script.py
@@ -23,4 +23,34 @@ def стоимость_разговора(operator):
     print(f"Оператор: {operator}, стоимость разговора: {cost_per_minute} руб ")
 
 
+#1 задание. Модуль 3. Циклы. Часть 3
+def ttt(x, y):
+    return x ** y
+
+#2 задание. Модуль 3. Циклы. Часть 3
+def count_same_digits():
+    count = 0
+    for number in range(100, 1000):
+        str_number = str(number)
+        if str_number[0] == str_number[1] or str_number[1] == str_number[2] or str_number[0] == str_number[2]:
+            count += 1
+        return count
+#3 задание. Модуль 3. Циклы. Часть 3
+def count_different_digits():
+    count = 0
+    for number in range(100, 10000):
+        str_number = str(number)
+        if len(set(str_number)) == len(str_number):
+            count += 1
+    return count
+
+#4 задание. Модуль 3. Циклы. Часть 3
+def remove_digits(number):
+    return number.replace("3", " ").replace("6"," ")
+
+
+
+
+
+
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from first_script import wqw, program, стоимость_разговора
+from first_script import wqw, program, стоимость_разговора, ttt, count_same_digits, count_different_digits, \
+    remove_digits
 
 number = int(input("Введите число от 1 до 100: "))
 wqw(number)
@@ -9,6 +10,23 @@ program(number, degree)
 
 operator = input("Введите оператора: ")
 стоимость_разговора(operator)
+
+x = int(input("Введите x: "))
+y= int(input("Введите y: "))
+
+result = x ** y
+print(f"{x} в степени {y} равно {result}")
+ttt(x, y)
+
+result = count_same_digits()
+print(f"Количествоо чисел с двумя одинаковыми знаками равно: {result}")
+
+result = count_different_digits()
+print(f"Количество с разными знаками: {result}")
+
+number = input("Введите целоееп число: ")
+result = remove_digits(number)
+print(f"число итоговое: {result}")
 
 
 
